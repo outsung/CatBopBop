72884321a940d47b3cf805582be8e898f2e41722
outsung <insung9546@gmail.com>
2020-12-14 00:15:55 +0900

    Complete

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 
 
+
+
 const BO = document.getElementById("bo");
 const OP = document.getElementById("op");
+
+const BO_sound = [];
+Array.from(new Array(12).keys()).map((i) => {
+    BO_sound.push(new Audio(`./sound/bo/bo (${i + 1}).wav`));
+});
+
+const OP_sound = [];
+Array.from(new Array(10).keys()).map((i) => {
+    OP_sound.push(new Audio(`./sound/op/op (${i + 1}).wav`));
+});
+
 
 const _bo = () => {
     BO.classList.add("on");
     OP.classList.remove("on");
+    BO_sound[Math.floor(Math.random() * (BO_sound.length + 1))].play();
 }
 const _op = () => {
     BO.classList.remove("on");
     OP.classList.add("on");
+    OP_sound[Math.floor(Math.random() * (OP_sound.length + 1))].play();
 }
 
 
